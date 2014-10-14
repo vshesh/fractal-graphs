93c78bd8ebbd66b3e581d20e588db14e58b70571
cayman.simpson <cayman.simpson@gmail.com>
2014-10-14 11:43:39 -0700

    Added list of statistics we need to compute

--- stats.py
+++ stats.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d5d6d76bb8a342b9895883ebaf9668d81465a6fa499ad47c969709c8dea0457b
-size 766
+oid sha256:8eec8955fb707a2e48afa134e2b91f21c41cedc38b6c06aae43b6b86a40b5258
+size 1198
