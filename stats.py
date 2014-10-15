37eea3e4d2b644ee07d2c67ff76f03bbd9d468ac
Vishesh Gupta <vishesh@stanford.edu>
2014-10-15 01:11:47 -0700

    merged the list where it should be on the file

--- stats.py
+++ stats.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d6fdd7ccf9fb85e4853414a669c72668493df72aff32aaac3ea1d5545342dc85
-size 1546
+oid sha256:0b8d147502737c666a4bd568cfae1322807d66c1c7758934994c656a9a3c03f9
+size 1211
