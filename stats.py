562eccc3daa4ec52b14e76157b8650bcc9dd6aab
cayman.simpson <cayman.simpson@gmail.com>
2014-10-15 11:39:19 -0700

    updating project proposal to rough draft

--- stats.py
+++ stats.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8eec8955fb707a2e48afa134e2b91f21c41cedc38b6c06aae43b6b86a40b5258
-size 1198
+oid sha256:0b8d147502737c666a4bd568cfae1322807d66c1c7758934994c656a9a3c03f9
+size 1211
