a2d8cf2529d94e2d99310073c542f2c5e34c49f5
Vishesh Gupta <vishesh@stanford.edu>
2014-10-15 01:11:07 -0700

    added list of stats

--- stats.py
+++ stats.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d5d6d76bb8a342b9895883ebaf9668d81465a6fa499ad47c969709c8dea0457b
-size 766
+oid sha256:7b810eea3d9d2ed3d08e2ef155b75a469746ee9de27d0c89eba8b2daac2899a3
+size 1114
