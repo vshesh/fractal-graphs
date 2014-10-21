f7c0250be2ed3ab4454ebed8799eef676dbb9f44
Vishesh Gupta <vishesh@stanford.edu>
2014-10-20 21:38:44 -0700

    fractal dimension works, but there are numerical problems

--- stats.py
+++ stats.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f0925deaef01b77e5e08dcc8335290206752b7082d483e9701ff6c5ba50fffa9
-size 3032
+oid sha256:7a74eef3703ac7df3e692c6c8bf4c07459451cb56d47614f90e0dc86dc71573c
+size 3489
