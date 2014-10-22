c905a55c6b9d8e3c3abde619a635e405c39b337f
Vishesh Gupta <vishesh@stanford.edu>
2014-10-21 20:32:42 -0700

    can now reduce graph

--- stats.py
+++ stats.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7a74eef3703ac7df3e692c6c8bf4c07459451cb56d47614f90e0dc86dc71573c
-size 3489
+oid sha256:badc000378869d9e1ed3ba5719324b4c651ab0f95a1e1e5d4ddb4f898fde3463
+size 5669
