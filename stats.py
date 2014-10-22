6371c2b70c797901f7a41bcc414dbbf2736a9a1e
Vishesh Gupta <vishesh@stanford.edu>
2014-10-21 22:06:01 -0700

    valfilter syntax bug

--- stats.py
+++ stats.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b66dce243fbf4174f6d02a783007364039d749a82361e44dc4985e65fa5f8ba1
-size 5926
+oid sha256:b06c73768fa110e05c8c432e5647a0711fb98622b2ff483372d71ea9ba16c901
+size 5922
