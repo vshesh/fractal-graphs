937353dda23bffcfef8b311fef51a7b488b283dc
Vishesh Gupta <vishesh@stanford.edu>
2014-10-23 13:18:45 -0700

    added divisions and organized the code a bit better

--- stats.py
+++ stats.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff5e382c60277de8096c7b45a776e112baf1fd5308a6aee48d15c79c9d68133f
-size 6311
+oid sha256:d05f5e262a1e4269e608aec9ba0329217f8a80a087bc0047d8f79ee6cf7b9581
+size 7343
