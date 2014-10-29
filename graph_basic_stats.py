2863da9febc11b08cf73a3c79e7045278b3b0775
Vishesh Gupta <vishesh@stanford.edu>
2014-10-29 16:53:07 -0700

    mathematica formatted output now, onto writing a presentation harness

--- graph_basic_stats.py
+++ graph_basic_stats.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e8f454e5e4983dbcaaccfd8ef9eaed2c28d4db506615bc3a0e582e50a6360c21
-size 7481
+oid sha256:9c2c85bdc92508dfcca03551a16a685a50dd483777850216bcdbd253d35a6030
+size 7513
--- stats.py
+++ stats.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:07bc70a9772d65c1417ab9f3b6014ce897783486f1eaa5a2ab06eab7ccb304d0
-size 9820
+oid sha256:fb3fdefea1fde551e1e27fb6589707cbdf35484572c1be2ebfd15f7f15ca07f5
+size 9995
