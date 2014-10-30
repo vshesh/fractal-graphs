c66833e7567a46650a64f5d1894c054c46808323
Vishesh Gupta <vishesh@stanford.edu>
2014-10-29 17:21:17 -0700

    fractal dimension should be positive

--- stats.py
+++ stats.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb3fdefea1fde551e1e27fb6589707cbdf35484572c1be2ebfd15f7f15ca07f5
-size 9995
+oid sha256:502c3b000289f392270dd625974d2121feca46229e0408aa0d4dbdcbd4ecb616
+size 10064
