759481f04b79ada8dbbe182d216443b304d7e170
Vishesh Gupta <vishesh@stanford.edu>
2014-10-31 18:21:23 -0700

    shifting towards purely using snap as the base graph representation

--- stats.py
+++ stats.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:018e72b4f2762a97fb32807c89a437ca8279ee0671ebe42993d326ba233e216f
-size 10164
+oid sha256:590beb4133a5cdd194704551b294fb76484495c76a2e3b2d5f01ee2fd2892cfc
+size 10467
