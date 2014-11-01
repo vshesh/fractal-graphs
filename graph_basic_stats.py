5b4dbc12f326d33a224ffb9157f469fdf2395935
Bharadwaj Raghavan <bharadr@Bharadwajs-MacBook-Pro.local>
2014-11-01 14:50:40 -0700

    Added a few more functions, hop plots function still in question

--- graph_basic_stats.py
+++ graph_basic_stats.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9c2c85bdc92508dfcca03551a16a685a50dd483777850216bcdbd253d35a6030
-size 7513
+oid sha256:144fcf0ea4dcf03ef1c6f17a69766f19ec2f63a8ad30aa2033326cba9fa0bf92
+size 9257
