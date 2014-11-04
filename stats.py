75e8662f3bab1a6b7068f3fc0c201d32d37c65aa
Vishesh Gupta <vishesh@stanford.edu>
2014-11-03 17:50:54 -0800

    removed some files

--- stats.py
+++ stats.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:590beb4133a5cdd194704551b294fb76484495c76a2e3b2d5f01ee2fd2892cfc
-size 10467
+oid sha256:0e248e17523be24f25df655618c8a94c280f39b5a1a221819e817bfe92bb59d6
+size 10261
