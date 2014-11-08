baf898c797c3f9dbe7a720e92336957a30c1f06c
Vishesh Gupta <vishesh@stanford.edu>
2014-11-07 20:39:42 -0800

    node triangle participation added, working on greedy box counting algorithm (supposedly very efficient)

--- stats.py
+++ stats.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0dd053866e6e9b07f8f865ff26da40c1e6738f4cf7094e542b44e32676d43940
-size 11395
+oid sha256:5afcec8c008a3d26e1c688f37b361abccd30bb30c24cb76d13953179e0c4c5c0
+size 12291
