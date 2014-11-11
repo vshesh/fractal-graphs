6b2f54679cf67d4af0ccde2a00bbda4800c949ec
Bharadwaj Raghavan <bharadr@dn0a182c57.sunet>
2014-11-11 11:27:21 -0800

    Finished the kronecker pipeline at last

---
+++ MGEOP_gen/Snap-2.3/examples/generate_kron.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8c62466159a182464bf46caa4bc7fb95403dc0d3e06a6991442606499348b2e1
+size 555
---
+++ MGEOP_gen/Snap-2.3/examples/get_matrix_params.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6d44f95cb5cbd39100fb4ae4dc3ecb948a8fb5e67e874463ce3d8ed813ca055c
+size 1379
