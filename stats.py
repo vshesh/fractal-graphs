3df03b9896b6b771cb345ec939842c5a9ecb1f6b
Vishesh Gupta <vishesh@stanford.edu>
2014-11-22 02:01:30 -0800

    added profile functions and am looking to handroll boxcounting; snap is an idiot.

--- stats.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:444e70db73846d6d396c6791e459be4416e1189e549ba554214a7cadf406a2eb
-size 13671
---
+++ stats/stats.c
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9a18ca1c847289ace260d38ff0e4a4bced44494cc4c9f2df294ee62ebf91e4a2
+size 914218
