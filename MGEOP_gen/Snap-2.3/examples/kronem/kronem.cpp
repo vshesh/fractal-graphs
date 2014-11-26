beb4e5e4d1eec192bcde40da307d63a378235f59
Bharadwaj Raghavan <bharadr@dn0a18176e.sunet>
2014-11-25 21:15:41 -0800

    Did graphgen on the smaller graphs

--- MGEOP_gen/Snap-2.3/examples/kronem/kronem.cpp
+++ MGEOP_gen/Snap-2.3/examples/kronem/kronem.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:615d73e1b1f43e22d7ffa86d9a6db8b4d37c6fa8215f7b3a9486265d2ba4bfee
-size 3733
+oid sha256:e50146b617e8c7fb73d0d51dd90e145ecaf6cf9c8dd96bdf0359b1ffc84039b9
+size 3859
---
+++ data/eColi/kr/eColi.CV.likelihood.plt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1c030e6b49bf1e5ecfd4501194dd570591b1bac69d71863ce6c9db1206c3df8a
+size 365
