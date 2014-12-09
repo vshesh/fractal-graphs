4a1f88c7880174d0452b0a2bfc98db4e1667dd2e
Vishesh Gupta <vishesh@stanford.edu>
2014-12-09 01:07:16 -0800

    added ./ in front of subprocess call

--- stats/stats.py
+++ stats/stats.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:243eaf776439c3d5548ee0932cc77e5af9c80a0fc8f7b631d1854186e44a0781
-size 12967
+oid sha256:8dcd1cd380cdab6a8914a747edbe27e85e0d556133ec2b0c77e921aea60ee4a9
+size 12969
